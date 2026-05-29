cc117ef0334dda8a30e5fce5c35782af6b396c1d
Rostya <rostyamudrik@proton.me>
2026-05-29 13:15:26 +0200

    login

--- main.py
+++ main.py
@@ -1,68 +1,31 @@
-from typing import Annotated
-
+from fastapi import FastAPI, HTTPException, Depends, Response
+from authx import AuthX, AuthXConfig
 from pydantic import BaseModel
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from fastapi import FastAPI, Depends
-
 
 app = FastAPI()
 
-engine = create_async_engine('sqlite+aiosqlite:///books.db')
+config = AuthXConfig
+config.JWT_SECRET_KEY = 'SECRET_KEY'
+config.JWT_ACCESS_COOKIE_NAME = 'my_access_token'
+config.JWT_TOKEN_LOCATION = ['cookies']
 
-new_session = async_sessionmaker(engine, expire_on_commit=False)
-
-
-async def get_session():
-    async with new_session as session:
-        yield session
+security = AuthX(config=config)
 
 
-SessionDep = Annotated[AsyncSession, Depends(get_session)]
+class UserLoginSchema(BaseModel):
+    username: str
+    password: str
 
 
-class Base(DeclarativeBase):
-    pass
+@app.post('/login')
+def login(creds: UserLoginSchema, response: Response):
+    if creds.username == 'text' and creds.password == 'text':
+        token = security.create_access_token(uid='12345')
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {'access_token': token}
+    raise HTTPException(status_code=401, detail='Incorrect username')
 
 
-class BookModel(Base):
-    __tablename__ = 'books'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str]
-    author: Mapped[str]
-
-
-@app.post('/setup_database')
-async def setup_database():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-    return {'ok': True}
-
-
-class BookAddSchema():
-    title: str
-    author: str
-
-
-class BookSchema(BookAddSchema):
-    id: int
-
-
-@app.post('/books')
-async def add_book(data: BookSchema, session: SessionDep):
-    new_book = BookModel(
-        title=data.title,
-        author=data.author,
-    )
-    session.add(new_book)
-    await session.commit()
-
-
-@app.get('/books')
-async def get_book(session: SessionDep):
-    query = select(BaseModel)
-    result = await session.execute(query)
-    return result.scalars().all()
+@app.get('protected', dependencies=[Depends(security.access_token_required)])
+def protected():
+    return {'data': 'TOP SECRET'}
