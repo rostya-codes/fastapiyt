430224b5510740b8c5ec3533ae445c7f0aef4040
Rostya <rostyamudrik@proton.me>
2026-05-30 13:48:18 +0200

    async and sync

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-from fastapi import FastAPI, HTTPException, Depends, Response
-from authx import AuthX, AuthXConfig
-from pydantic import BaseModel
+import time
+import asyncio
+
+from fastapi import FastAPI, BackgroundTasks
 
 app = FastAPI()
 
-config = AuthXConfig
-config.JWT_SECRET_KEY = 'SECRET_KEY'
-config.JWT_ACCESS_COOKIE_NAME = 'my_access_token'
-config.JWT_TOKEN_LOCATION = ['cookies']
 
-security = AuthX(config=config)
+def sync_task():
+    time.sleep(3)
+    print('Отправлен email')
 
 
-class UserLoginSchema(BaseModel):
-    username: str
-    password: str
+async def async_task():
+    await asyncio.sleep(3)
+    print('Сделан запрос в сторонний API')
 
 
-@app.post('/login')
-def login(creds: UserLoginSchema, response: Response):
-    if creds.username == 'text' and creds.password == 'text':
-        token = security.create_access_token(uid='12345')
-        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
-        return {'access_token': token}
-    raise HTTPException(status_code=401, detail='Incorrect username')
-
-
-@app.get('protected', dependencies=[Depends(security.access_token_required)])
-def protected():
-    return {'data': 'TOP SECRET'}
+@app.post('/')
+async def some_route(bg_tasks: BackgroundTasks):
+    ...
+    bg_tasks.add_task(sync_task)
+    return {'ok': True}
